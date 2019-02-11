be05dcc805a7cf15e554c10d3f8b1a83cce427e1
Alex <alex.drysdale96@gmail.com>
2019-02-11 12:42:00 +0000

    Improved GUI

--- ct_reconstruction.py
+++ ct_reconstruction.py
@@ -12,21 +12,24 @@
 #Imports relevent libraries
 from skimage import io
 import numpy as np
+import os
 from scipy import ndimage as ndi
 import skimage.transform as skt
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 from tkinter import Tk, Label, filedialog,Button
 from tkinter import *
 from tkinter.ttk import Progressbar
-
-
-
+import pygame.mixer as mxr
 
 
 #Initialises the window (GUI)
 window = Tk()
-window.title("Rotational Reconstruction")
-window.geometry('400x100')
+window.title("Filtered Back Projection")
+window.geometry('400x400')
+mxr.init()
 
 #Creates progress bar
 bar = Progressbar(window, length=400)
@@ -50,24 +53,80 @@ def get_image(window):
     wtxt_select.grid(column=0,row=0)
     window.update()
 
-    return img_object
+    return img_object,str_object
+
+#Obtains the folder for the filtered images to be saved to
+def dir_save_path():
+
+	#Opens the file containg the save path
+	save_path="save_path.txt"
+
+	#Creates the text file if non-existent
+	if os.path.exists(save_path)==False:
+		file=open(save_path,"w")
+		file.close()
+
+	#Opens the file to read in the save dir name
+	file=open(save_path,"r")
+	dir_path=file.readline()
+
+	#Asks the user to select the folder if one is not pre saved
+	if os.path.isdir(dir_path)==False:
+
+		#Asks user to select a save location
+		no_folder=Label(window,text="Please a save folder.")
+		no_folder.grid(column=0,row=0)
+		window.update()
+
+		#Obtains the user selected folder
+		dir_path=filedialog.askdirectory()
+
+		#Informs user of update
+		no_folder=Label(window, text="           Ta.            ")
+		no_folder.grid(column=0,row=0)
+		window.update()
+
+	#Writes the result to the save_path file
+	file.close()
+	file=open(save_path,"w")
+	file.write(dir_path)
+	file.close()
+
+	return dir_path
+
+#Obtains just the file name from a whole path
+def file_name(path):
+
+	#Finds the starting index of the file
+	for i in range(0,len(path)):
+		if path[i]=="/":
+			dir_index=i
+
+	#Returns only the file name
+	image_name=path[dir_index+1:len(path)]
+	return image_name
 
 def main():
 
     #Acquires object from user and creates canvas for 3D image
-    obj_stack = get_image(window)
+    obj_stack,str_object = get_image(window)
     dim_stack = obj_stack.shape
     obj_fbp= np.zeros((dim_stack[1],dim_stack[1],dim_stack[1])) #As image is square
 
-    #Creates slider for slice selection
-    slider = Scale(window, from_=0, to=dim_stack[1]-1, orient=HORIZONTAL)
-    slider.grid(column=0,row=30)
-    window.update()
+    #Gets save location from user (if needed)
+    dir_save = dir_save_path()
+    str_imname = file_name(str_object)
+    str_save = os.path.join(dir_save, str_imname)
 
     #Creates theta array (in degrees)
     dtheta = 1
     theta = np.arange(0, dim_stack[0],dtheta)
 
+    #Starting loading music
+    mxr.music.load("musac.mp3")
+    mxr.music.play(start=1.3)
+
+    #Obtains reconstruction using filtered back projection
     for i in range(0,dim_stack[1]):
 
         #Updates progress bar
@@ -75,22 +134,38 @@ def main():
         bar['value'] = percentage
         window.update()
 
+        #Fades out music
+        if percentage == 92:
+            mxr.music.fadeout(7000)
+
         #Performs filtered back projection on each y slice
-        obj_fbp[:,i,:] = skt.iradon(ndi.rotate(obj_stack[:,i,:],90),theta=theta,circle=True)
+        obj_fbp[i,:,:] = skt.iradon(ndi.rotate(obj_stack[:,i,:],90),theta=theta,circle=True)
+
+
 
     #Saves the image
-    io.imsave('test.tif',obj_fbp)
+    print(dir_save+"\n"+str_imname +"\n"+str_save)
+    io.imsave(str_save,obj_fbp.astype('uint32'),plugin='tifffile')
 
-    #Button to display image
-    #Displays new slice
-    def clicked():
-        plt.imshow(obj_fbp[:,slider.get(),:])
-        plt.show()
+    #Plots the figure
+    fig = Figure(figsize=(3,3))
+    a = fig.add_subplot(111)
+    a.imshow(obj_fbp[0,:,:])
 
-    #Creates display button
-    btn = Button(window, text="Load slice",bg="green",fg="white", command=clicked)
-    btn.grid(column=0, row=20)
-    window.update()
+    #Draws to tkinter
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.get_tk_widget().grid(row=60,column=0)
+    canvas.draw()
+
+    #Updates image command
+    def slider_moved(val):
+        a.clear()
+        a.imshow(obj_fbp[int(val),:,:])
+        canvas.draw()
+
+    #Adds slider to select plane
+    slider = Scale(window,from_=0, to=dim_stack[1]-1,orient=HORIZONTAL,command=slider_moved)
+    slider.grid(row=30,column=0)
 
     window.mainloop()
 
